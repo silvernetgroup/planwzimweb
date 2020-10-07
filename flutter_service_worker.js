f9071b949638a98c73d8aebe53a1b4ae2709ce81
Matyjasiak Bartosz <matyjasiakbartosz@gmail.com>
2020-10-07 16:07:28 +0200

    options redesign 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "335488936e1614d415f3ffa43148ebf6",
-"/": "335488936e1614d415f3ffa43148ebf6",
-"main.dart.js": "c1ce483467200383a762721c86e5d754",
+"index.html": "4c8c7d69b1153c6e7ec0b014705d4c54",
+"/": "4c8c7d69b1153c6e7ec0b014705d4c54",
+"main.dart.js": "a9847017d222b6a7d2db4ee492670943",
 "manifest.json": "aae197d99a4d906b3800e8221c8c71e8"
 };
 
